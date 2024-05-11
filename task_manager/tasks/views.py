a28453eafd5405e94251df6b6c3eb99756dfb3e7
farhanalise <farhan.ali.se@gmail.com>
2024-05-11 06:27:25 +0100

    [ADD]defined views for Task and Label CRUD operations.

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Task, Label
+from .serializers import TaskSerializer, LabelSerializer
 
-# Create your views here.
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
+
+class TaskList(generics.ListCreateAPIView):
+    # queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Filter tasks based on authenticated user
+        return Task.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+
+class LabelList(generics.ListCreateAPIView):
+    # queryset = Label.objects.all()
+    serializer_class = LabelSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Filter labels based on authenticated user
+        return Label.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class LabelDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Label.objects.all()
+    serializer_class = LabelSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
