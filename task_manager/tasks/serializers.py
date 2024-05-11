368c8874f39f687ec20b7744cb2574d1b98d9ba6
farhanalise <farhan.ali.se@gmail.com>
2024-05-11 06:26:17 +0100

    [ADD]created seriializers for Task and Label. this will allow us to convert model instances to JSON and vice versa

---
+++ task_manager/tasks/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Task, Label
+
+
+class LabelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Label
+        fields = '__all__'
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
